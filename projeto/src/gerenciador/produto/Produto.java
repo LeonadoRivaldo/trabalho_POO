946a53c52f27204330cd7420eb45adb7ba23fed9
Leonardo Rivaldo <leonardo.rivaldo@gmail.com>
2016-10-17 16:50:59 -0200

    asd
    asd

--- projeto/src/gerenciador/produto/Produto.java
+++ projeto/src/gerenciador/produto/Produto.java
@@ -10,5 +10,49 @@ package gerenciador.produto;
  * @author leona_000
  */
 public abstract class Produto {
-    
+
+    private int codigo;
+    private double preco;
+    private String nome, descricao;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        str += "\nNome:" + nome;
+        str += "\nDescricao: " + descricao;
+        return "Produto{" + "codigo=" + codigo + ", preco=" + preco;
+    }
+
 }
